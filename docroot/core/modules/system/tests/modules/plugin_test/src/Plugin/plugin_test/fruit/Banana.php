090af29be599fd619bcf027ab96d05cc6adc3f5e
kundan81 <neeraj4azad@rediffmail.com>
2016-06-08 13:35:27 +0530

    Initial Commit

---
+++ docroot/core/modules/system/tests/modules/plugin_test/src/Plugin/plugin_test/fruit/Banana.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\plugin_test\Plugin\plugin_test\fruit\Banana.
+ */
+
+namespace Drupal\plugin_test\Plugin\plugin_test\fruit;
+
+/**
+ * @Plugin(
+ *   id = "banana",
+ *   label = "Banana",
+ *   color = "yellow",
+ *   uses = {
+ *     "bread" = @Translation("Banana bread"),
+ *     "loaf" = @PluralTranslation(
+ *       singular = "@count loaf",
+ *       plural = "@count loaves"
+ *     )
+ *   }
+ * )
+ */
+class Banana implements FruitInterface {
+
+}
